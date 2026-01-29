1510f880caeab93b1b1e36ab02933189e9b0b2f6
Aleksander <korkh1979@gmail.com>
2026-01-29 14:57:14 +0100

    Tightning httpHeaders. Updated dockerfile, small corrections

---
+++ API/Data/Migrations/20260129123337_FixPendingChanges.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "ca5f165d-7bba-434f-9fc2-15b5754af11a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "4313f44d-9683-40b9-bafb-0db22c9a9430");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: null);
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -7,65 +7,63 @@ using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+builder.Services.AddEndpointsApiExplorer();
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    var jwtSecurityScheme = new OpenApiSecurityScheme
-    {
-        BearerFormat = "JWT",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = JwtBearerDefaults.AuthenticationScheme,
-        Description = "Put Bearer + your token in the box below",
-        Reference = new OpenApiReference
-        {
-            Id = JwtBearerDefaults.AuthenticationScheme,
-            Type = ReferenceType.SecurityScheme
-        }
-    };
-    c.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
 
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            jwtSecurityScheme, Array.Empty<string>()
-        }
-    });
-});
 
 string connString;
 if (builder.Environment.IsDevelopment())
+{
     connString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
 else
 {
     // Use connection string provided at runtime by FlyIO.
     var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-    // Parse connection URL to connection string for Npgsql
-    connUrl = connUrl.Replace("postgres://", string.Empty);
-    var pgUserPass = connUrl.Split("@")[0];
-    var pgHostPortDb = connUrl.Split("@")[1];
-    var pgHostPort = pgHostPortDb.Split("/")[0];
-    var pgDb = pgHostPortDb.Split("/")[1];
-    var pgUser = pgUserPass.Split(":")[0];
-    var pgPass = pgUserPass.Split(":")[1];
-    var pgHost = pgHostPort.Split(":")[0];
-    var pgPort = pgHostPort.Split(":")[1];
-    var updatedHost = pgHost.Replace("flycast", "internal");
+    if (string.IsNullOrEmpty(connUrl))
+    {
+        // This will show up clearly in your 'fly logs'
+        throw new InvalidOperationException("DATABASE_URL environment variable is empty. Check if Postgres is attached.");
+    }
 
-    connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+    try 
+    {
+        // Parse connection URL to connection string for Npgsql using Uri class
+        var databaseUri = new Uri(connUrl);
+        var userInfo = databaseUri.UserInfo.Split(':');
+
+        var pgUser = userInfo[0];
+        var pgPass = userInfo[1];
+        var pgHost = databaseUri.Host;
+        var pgPort = databaseUri.Port;
+        var pgDb = databaseUri.LocalPath.TrimStart('/');
+
+        // Fly.io specific: internal networking
+        var updatedHost = pgHost.Replace("flycast", "internal");
+
+        // В Program.cs найди строку формирования connString и замени на эту:
+        connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};Trust Server Certificate=true;";
+    }
+    catch (Exception ex)
+    {
+        throw new Exception("Error parsing DATABASE_URL. Make sure it's in the correct format.", ex);
+    }
 }
+
 builder.Services.AddDbContext<StoreContext>(opt =>
 {
+    opt.ConfigureWarnings(w => w.Ignore(RelationalEventId.PendingModelChangesWarning));
     opt.UseNpgsql(connString);
 });
 
@@ -74,12 +72,19 @@ builder.Services.AddIdentityCore<User>(opt =>
 {
     opt.User.RequireUniqueEmail = true; //avoid duplicates for email
     opt.SignIn.RequireConfirmedEmail = false;
-})
+    })
     .AddRoles<Role>()
     .AddEntityFrameworkStores<StoreContext>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(opt =>
-    {
+    .AddJwtBearer(opt => {
+        opt.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                context.Token = context.Request.Cookies["jwt"];
+                return Task.CompletedTask;
+            }
+        };
         opt.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = false,
@@ -101,42 +106,80 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseDefaultFiles(); //searches inside wwwwroot for idex.html to serve
-app.UseStaticFiles();
+app.UseXContentTypeOptions();
 
-app.UseCors(opt =>
-{
-    opt
-        .AllowAnyMethod()
-        .AllowAnyHeader()
-        .AllowCredentials()
-        .WithExposedHeaders("WWW-Authenticate", "Pagination")
-        .WithOrigins("http://localhost:3000", "https://localhost:3000", "http://localhost:5000");
-});
+app.UseXXssProtection(Options => Options.EnabledWithBlockMode());
+app.UseXfo(opt => opt.Deny()); //against click-hijacking
+app.UseCsp(opt => opt
+    .BlockAllMixedContent()
+    .StyleSources(s => s.Self()
+        .CustomSources("https://fonts.googleapis.com", "https://checkout.stripe.com")
+        .UnsafeInline() 
+    )
+    .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
+    .FormActions(s => s.Self())
+    .FrameAncestors(s => s.Self())
+    // Добавляем Stripe в скрипты и фреймы
+    .ScriptSources(s => s.Self()
+        .CustomSources("https://js.stripe.com", "https://checkout.stripe.com")
+    )
+    .FrameSources(s => s.Self()
+        .CustomSources("https://js.stripe.com", "https://checkout.stripe.com")
+    )
+    .ImageSources(s => s.Self()
+        .CustomSources("blob:", "data:", "https://res.cloudinary.com", "https://*.stripe.com")
+    )
+);
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
+    app.MapOpenApi();
+}
+else
+{
+    // Включаем HSTS (уже есть ручной заголовок, но лучше использовать стандарт)
+    app.UseHsts(); 
+    
+    // Обязательно добавляем редирект с http на https
+    app.UseHttpsRedirection(); 
+
+    app.Use(async (context, next) =>
     {
-        c.ConfigObject.AdditionalItems.Add("persistAuthorization", "true");
+        context.Response.Headers.Remove("X-Powered-By");
+        context.Response.Headers.Append("Permissions-Policy", "camera=(), microphone=(), geolocation=()");
+        context.Response.Headers.Append("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
     });
 }
 
+
+
+app.UseReferrerPolicy(options => options.NoReferrer());
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 
 app.MapFallbackToController("index", "Fallback"); // auto-recognizing routes
 
-var scope = app.Services.CreateScope();
+using var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
 var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
 var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
 
 try
 {
+    logger.LogInformation("Starting migrations..."); 
     await context.Database.MigrateAsync();
+    logger.LogInformation("Migrations applied successfully!"); 
+    
     await DbInitializer.Initialize(context, userManager);
+    logger.LogInformation("Database seeding completed!");
 }
 catch (Exception ex)
 {
